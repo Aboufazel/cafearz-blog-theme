8317a51910b3249ebcdb7399cd7230f71f821344
FrontEnd <a.ab2521@gmail.com>
2023-04-30 15:06:31 +0330

    feat : tailwind config extend color add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                black: "#21294D",
+                date: "#898989",
+                gold: "#F0B90B",
+                hint: "#c5c5c5",
+                blogText: "#c4c4c4",
+                dataBg: "#F4F7FE",
+                blue: "#3377E8",
+                zeroBlack: "#000000",
+                secondary: "#F3F6FD",
+                third: "#F2F5FC",
+                primaryText: "#4A517D",
+                secondaryText: "rgba(26, 28, 32, 0.5)",
+                blackText: "#21294D",
+                duringBox: "#F2F2F2",
+                linerBg: "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #F7F8FD 100%)",
+                secondaryButtonHover: "#EEB60D",
+            },
+        },
+    },
+    plugins: [],
 }
 
